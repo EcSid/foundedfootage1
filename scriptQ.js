1858b04c5b4c768d8207a2b871c4e222d7ede5fc
EcSid <166507541+EcSid@users.noreply.github.com>
2024-04-09 14:17:54 +0300

    Add files via upload

---
+++ scriptQ.js
@@ -0,0 +1,3 @@
+setTimeout(() => document.querySelector("p").classList.remove("hidden"), 1000);
+setTimeout(() => document.querySelector(".button").classList.remove("hidden"), 1800);
+setTimeout(() => document.querySelector(".button2").classList.remove("hidden"), 1800);
